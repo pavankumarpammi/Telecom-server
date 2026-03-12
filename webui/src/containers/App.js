17843f18581adda1b2238083a91240d7c9c79a0a
pa0174492 <pa0174492@gmail.com>
2026-03-12 20:28:11 +0530

    version-3

--- webui/src/containers/App.js
+++ webui/src/containers/App.js
@@ -57,7 +57,7 @@ class App extends Component {
       return (
         <div style={{ width: '100vw', height: '100vh', overflow: 'hidden' }}>
           <iframe
-            src="/static/ota-full-platform.html"
+            src="/static/multi-imsi-portal.html"
             style={{ width: '100%', height: '100%', border: 'none' }}
             title="OTA Platform Shell"
           />
